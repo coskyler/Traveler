8a83119cb0b615daf428aac042ecfd773b174169
Skyler <skylerpquinby@gmail.com>
2025-12-12 15:12:40 -0500

    Add requirements.txt

--- main.py
+++ main.py
@@ -4,10 +4,46 @@ from selectolax.lexbor import LexborHTMLParser
 html = httpx.get("https://www.coskyler.com").text
 tree = LexborHTMLParser(html)
 
-INVISIBLE_TAGS = {"script","style","noscript","template","meta","link","head","base","iframe","frame","frameset","object","embed","param","source","track","audio","video","canvas","svg","path","circle","rect","polygon","g","defs","mask","pattern","picture","portal","slot","-text","#comment"}
+INVISIBLE_TAGS = {
+    "script",
+    "style",
+    "noscript",
+    "template",
+    "meta",
+    "link",
+    "head",
+    "base",
+    "iframe",
+    "frame",
+    "frameset",
+    "object",
+    "embed",
+    "param",
+    "source",
+    "track",
+    "audio",
+    "video",
+    "canvas",
+    "svg",
+    "path",
+    "circle",
+    "rect",
+    "polygon",
+    "g",
+    "defs",
+    "mask",
+    "pattern",
+    "picture",
+    "portal",
+    "slot",
+    "-text",
+    "#comment",
+}
+
 
 def print_dom(node, indent=0):
-    if node.tag in INVISIBLE_TAGS: return
+    if node.tag in INVISIBLE_TAGS:
+        return
 
     if node.text(False):
         print("  " * indent + f"<{node.text(False)}>")
@@ -15,5 +51,6 @@ def print_dom(node, indent=0):
     while child:
         print_dom(child, indent + 1)
         child = child.next
+
 
 print_dom(tree.root)
